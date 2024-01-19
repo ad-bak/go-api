d5500436334d5c462a145f11a6ad0b6de4d75760
Adnan <adbak.dev@gmail.com>
2024-01-20 00:48:02 +0100

    getting started

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Hello!",
+		})
+	})
+
+	server.Run(":8080")
+
+}
