50a883c85bae52ea2c13f3955fce93be59eda964
Adnan <adbak.dev@gmail.com>
2024-01-20 21:41:14 +0100

    sql

--- main.go
+++ main.go
@@ -3,18 +3,39 @@ package main
 import (
 	"net/http"
 
+	"example.com/books/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Hello!",
-		})
-	})
+	server.GET("/", getEvent)
+
+	server.POST("/", createEvent)
 
 	server.Run(":8080")
 
+}
+
+func getEvent(ctx *gin.Context) {
+	events := models.GetAllEvents()
+	ctx.JSON(http.StatusOK, events)
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+	err := ctx.ShouldBindJSON(&event)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Could not parse requested data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
